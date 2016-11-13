702c15affd6670a20a3e2b8e5e4755e02f4d62e9
shadbolt <linux4me>
2016-11-13 16:24:27 -0500

    Initial push

---
+++ app/src/main/java/com/ssj/shadbolt/smolovjrcompanion/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ssj.shadbolt.smolovjrcompanion.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ssj.shadbolt.smolovjrcompanion.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
